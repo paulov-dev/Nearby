cff7dca7b8590e3441cfb6cdd2d3645c76e78e02
Paulo Victor <paulovmmf@gmail.com>
2024-12-15 16:19:51 -0300

    add map

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { View, Alert } from "react-native";
+import * as Location from 'expo-location';
+import MapView from "react-native-maps";
+
 
 import { api } from "@/services/api";
 
@@ -11,9 +14,18 @@ export default function Home(){
 
     type MarketsProps = PlaceProps
 
+    type Location = {
+        latitude: number,
+        longitude: number
+    }
+
     const [categories, setCategories] = useState<CategoriesProps>([])
     const [category, setCategory] = useState("")
     const [markets, setMarkets] = useState<MarketsProps[]>([])
+    const [location, setLocation] = useState<Location>({
+        latitude: 0,
+        longitude: 0,
+    });
 
     async function fetchCategories() {
         try {
@@ -40,7 +52,24 @@ export default function Home(){
         }        
     }
 
+    async function getCurrentLocation() {
+        try {
+            const { granted } = await Location.requestForegroundPermissionsAsync();
+            if(granted){
+                const getLocation = await Location.getCurrentPositionAsync()
+                const locationFunction = {
+                    latitude: getLocation.coords.latitude,
+                    longitude: getLocation.coords.longitude
+                }
+                setLocation(locationFunction)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     useEffect(() => {
+        getCurrentLocation()
         fetchCategories()
     }, [])
 
@@ -53,6 +82,15 @@ export default function Home(){
             data={categories} 
             onSelect={setCategory} 
             selected={category} 
+        />
+
+        <MapView style={{ flex: 1 }} 
+            region={{
+                latitude: location.latitude,
+                longitude: location.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+            }}
         />
 
         <Places data={markets} />
